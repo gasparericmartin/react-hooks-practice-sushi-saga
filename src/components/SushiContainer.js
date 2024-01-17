f6df297a0cce327f01455081191e65d2a23c3dff
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-01-17 13:25:18 -0500

    Fetched data and assigned to state variable

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi'
 
 function SushiContainer(props) {
+  const [sushiList, setSushiList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/sushis')
+    .then((response) => response.json())
+    .then((data) => setSushiList(data))
+  }, [])
+
+  
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
